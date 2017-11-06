1c4b9f5800cbff9d6c555fc4ba0159f278dae671
Dave Lambley <github@davel.me.uk>
2017-11-06 20:26:46 +0000

    Fix erasing.

--- display.pl
+++ display.pl
@@ -10,13 +10,13 @@ use SDLx::App;
 use SDL::Surface;
 use SDL::Rect;
 use SDL::Image;
+use SDL::Joystick;
 use JSON::XS qw/ decode_json /;
 use POSIX ":sys_wait_h";
 use Time::HiRes qw/ sleep /;
 
 pipe(my $command_r, my $command_w) or die $!;
 pipe(my $resp_r, my $resp_w)       or die $!;
-
 
 my $pid = fork() // die $!;
 if (!$pid) {
@@ -30,6 +30,10 @@ if (!$pid) {
 
 close $command_w;
 close $resp_r;
+
+#SDL::init_sub_system(SDL_INIT_JOYSTICK);
+
+#my @joystick = (SDL::Joystick->new(0));
 
 my $app = SDLx::App->new(
     title  => "Pretend GraphWin",
@@ -50,10 +54,37 @@ my @objects;
 while (my $line = <$command_r>) {
     chomp $line;
     my $query = decode_json($line);
+
+    SDL::Joystick::update();
+
+    if ($query->{action} eq 'init') {
+        next;
+    }
+    elsif ($query->{action} eq 'writeln') {
+        print $query->{string};
+        print "\n";
+    }
+    elsif ($query->{action} eq 'drawoblong') {
+        plot_obj($query);
+        SDL::Video::update_rects($app, $bg);
+        push @objects, $query;
+    }
+    elsif ($query->{action} eq 'eraseoblong') {
+        COMP: for my $i (0..scalar(@objects)-1) {
+            for my $h (qw/ x1 x2 y1 y2 penr peng penb brushr brushg brushb style /) {
+                next COMP if $query->{$h} ne $objects[$i]->{$h};
+            }
+            splice(@objects, $i, 1);
+            last;
+        }
+        refresh();
+    }
+    elsif ($query->{action} eq 'keypressed') {
+    }
+    else {
+    }
  
-    plot_obj($query);
- 
-    SDL::Video::update_rects($app, $bg);
+    #sleep 0.01;
 }
 
 waitpid($pid, 0) or die $!;
@@ -61,10 +92,7 @@ waitpid($pid, 0) or die $!;
 
 sub plot_obj {
     my ($obj) = @_;
-    if ($obj->{action} eq 'init') {
-        return;
-    }
-    elsif ($obj->{action} eq 'drawoblong') {
+    if ($obj->{action} eq 'drawoblong') {
         SDL::GFX::Primitives::box_RGBA(
             $app,
             $obj->{x2},
@@ -91,5 +119,22 @@ sub plot_obj {
     else {
         die "unimplemented ".$obj->{action};
     }
+    return;
+}
+
+sub refresh {
+    my $bg = SDL::Rect->new(0, 0, 800, 600);
+    SDL::Video::fill_rect(
+        $app,
+        $bg,
+        SDL::Video::map_RGB($app->format, 255, 255, 255),
+    );
+
+    for my $obj (@objects) {
+        plot_obj($obj);
+    }
+
+    SDL::Video::update_rects($app, $bg);
+
     return;
 }
