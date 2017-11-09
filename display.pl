baf54b61ba628d473c9f6ee379fca5262aa67937
Dave Lambley <github@davel.me.uk>
2017-11-09 17:27:35 +0000

    Loop and exit.

--- display.pl
+++ display.pl
@@ -18,22 +18,6 @@ use List::Util qw/ max /;
 
 my $game = $ARGV[0];
 
-pipe(my $command_r, my $command_w) or die $!;
-pipe(my $resp_r, my $resp_w)       or die $!;
-
-my $pid = fork() // die $!;
-if (!$pid) {
-    close $command_r;
-    close $resp_w;
-
-    open(STDOUT, ">&", $command_w) or die $!;
-    open(STDIN,  "<&", $resp_r)    or die $!;
-    exec($ARGV[0]) or die $!;
-}
-
-close $command_w;
-close $resp_r;
-
 SDL::init_sub_system(SDL_INIT_JOYSTICK);
 
 my $key;
@@ -80,95 +64,123 @@ printf("Number of Axes: %d\n",    SDL::Joystick::num_axes($joystick[1]));
 my @button_time;
 my @button;
 
-my $bg = SDL::Rect->new(0, 0, 800, 600);
-SDL::Video::fill_rect(
-    $app,
-    $bg,
-    SDL::Video::map_RGB($app->format, 255, 255, 255),
-);
+my $pid;
+my $exit;
+
+$SIG{INT} = sub {
+    kill(15, $pid) if $pid;
+    $exit = 1;
+};
 
 my @objects;
 
-my $text_y = 0;
-my $text_height = 20;
+while (!$exit) {
+    my $bg = SDL::Rect->new(0, 0, 800, 600);
+    SDL::Video::fill_rect(
+        $app,
+        $bg,
+        SDL::Video::map_RGB($app->format, 255, 255, 255),
+    );
 
-while (my $line = <$command_r>) {
-    chomp $line;
-    my $query = decode_json($line);
+    @objects = ();
+    my $text_y = 0;
+    my $text_height = 20;
 
-    if ($query->{action} eq 'init') {
-        next;
+    pipe(my $command_r, my $command_w) or die $!;
+    pipe(my $resp_r, my $resp_w)       or die $!;
+
+    my $pid = fork() // die $!;
+    if (!$pid) {
+        close $command_r;
+        close $resp_w;
+
+        open(STDOUT, ">&", $command_w) or die $!;
+        open(STDIN,  "<&", $resp_r)    or die $!;
+        exec($ARGV[0]) or die $!;
     }
-    elsif ($query->{action} eq 'writeln') {
-        my %str = (
-            action  => 'drawtext',
-            x       => 0,
-            y       => $text_y,
-            penr    => 0,
-            peng    => 0,
-            penb    => 0,
-            string  => $query->{string},
-        );
-        plot_obj(\%str);
-        SDL::Video::update_rects($app, $bg);
-        push @objects, \%str;
-        $text_y += $text_height;
-    }
-    elsif ($query->{action} eq 'drawtext') {
-        plot_obj($query);
-        push @objects, $query;
-    }
-    elsif ($query->{action} eq 'drawoblong') {
-        plot_obj($query);
-        push @objects, $query;
-    }
-    elsif ($query->{action} eq 'eraseoblong' || $query->{action} eq 'erasetext') {
-        my $s = $query->{action};
-        $s =~ s/erase/draw/;
-        my $erase;
-        COMP: for my $i (0..scalar(@objects)-1) {
-            next COMP if $s ne $objects[$i]->{action};
-            for my $h (qw/ x1 x2 y1 y2 x y string penr peng penb brushr brushg brushb style /) {
-                next if !defined($query->{$h}) && !defined($objects[$i]->{$h});
-                next COMP if $query->{$h} ne $objects[$i]->{$h};
+
+    close $command_w;
+    close $resp_r;
+
+
+    while (my $line = <$command_r>) {
+        chomp $line;
+        my $query = decode_json($line);
+
+        if ($query->{action} eq 'init') {
+            next;
+        }
+        elsif ($query->{action} eq 'writeln') {
+            my %str = (
+                action  => 'drawtext',
+                x       => 0,
+                y       => $text_y,
+                penr    => 0,
+                peng    => 0,
+                penb    => 0,
+                string  => $query->{string},
+            );
+            plot_obj(\%str);
+            SDL::Video::update_rects($app, $bg);
+            push @objects, \%str;
+            $text_y += $text_height;
+        }
+        elsif ($query->{action} eq 'drawtext') {
+            plot_obj($query);
+            push @objects, $query;
+        }
+        elsif ($query->{action} eq 'drawoblong') {
+            plot_obj($query);
+            push @objects, $query;
+        }
+        elsif ($query->{action} eq 'eraseoblong' || $query->{action} eq 'erasetext') {
+            my $s = $query->{action};
+            $s =~ s/erase/draw/;
+            my $erase;
+            COMP: for my $i (0..scalar(@objects)-1) {
+                next COMP if $s ne $objects[$i]->{action};
+                for my $h (qw/ x1 x2 y1 y2 x y string penr peng penb brushr brushg brushb style /) {
+                    next if !defined($query->{$h}) && !defined($objects[$i]->{$h});
+                    next COMP if $query->{$h} ne $objects[$i]->{$h};
+                }
+                splice(@objects, $i, 1);
+                $erase = 1;
+                last;
             }
-            splice(@objects, $i, 1);
-            $erase = 1;
-            last;
+            if (!$erase) {
+                die "could not erase $line";
+            }
+            refresh();
         }
-        if (!$erase) {
-            die "could not erase $line";
+        elsif ($query->{action} eq 'keypressed') {
+            $key ||= joystick();
+             syswrite($resp_w, $key ? "1\n":"0\n") or die $!;
         }
-        refresh();
-    }
-    elsif ($query->{action} eq 'keypressed') {
-        $key ||= joystick();
-         syswrite($resp_w, $key ? "1\n":"0\n") or die $!;
-    }
-    elsif ($query->{action} eq 'readkey') {
-        syswrite($resp_w, "$key\n") or die $!;
-        $key = "";
-    }
-    elsif ($query->{action} eq 'sleep') {
-        my $t0 = [gettimeofday];
-        SDL::Video::update_rects($app, $bg);
+        elsif ($query->{action} eq 'readkey') {
+            syswrite($resp_w, "$key\n") or die $!;
+            $key = "";
+        }
+        elsif ($query->{action} eq 'sleep') {
+            my $t0 = [gettimeofday];
+            SDL::Video::update_rects($app, $bg);
 
-        sleep(max($query->{duration}-tv_interval($t0), 0));
-        syswrite($resp_w, "x\n") or die $!;
-    }
+            sleep(max($query->{duration}-tv_interval($t0), 0));
+            syswrite($resp_w, "x\n") or die $!;
+        }
 
 
-    else {
-        die "No action! ".$query->{action};
+        else {
+            die "No action! ".$query->{action};
+        }
+     
+        #sleep 0.01;
     }
- 
-    #sleep 0.01;
+
+    sleep 2;
+
+    waitpid($pid, 0) or die $!;
+    $pid = undef;
 }
-
-sleep 2;
-
-waitpid($pid, 0) or die $!;
-
 
 sub plot_obj {
     my ($obj) = @_;
